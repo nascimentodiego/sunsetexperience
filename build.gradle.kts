6df91da405880e105b3006d8c5f85847480b9c77
Diego Nascimento <nascimento.diegof@gmail.com>
2023-03-17 13:18:36 -0300

    [CONFIG] init project.

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    extra.apply {
+        set("composeVersion", "1.6.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
