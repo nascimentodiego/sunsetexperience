fd8a7e9320078a2a44079dec57f20364c6a2f5bb
Diego Nascimento <nascimento.diegof@gmail.com>
2023-05-23 11:06:26 -0300

    test pre push.

--- app/src/main/java/br/com/dfn/app/sunsetexp/MainActivity.kt
+++ app/src/main/java/br/com/dfn/app/sunsetexp/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SunsetExperienceTheme {
-                // A surface container using the 'background' color from the theme
+                    // A surface container using the 'background' color from the theme
                                 Surface(
             modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
