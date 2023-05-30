b748b83419d77cfdca4f8c38b378f9303ab28760
Diego Nascimento <nascimento.diegof@gmail.com>
2023-05-29 21:30:39 -0300

    Feature/bitrise init (#4)
    * Add bitrise file.
    
    * Testing...

--- app/src/test/java/br/com/dfn/app/sunsetexp/ExampleUnitTest.kt
+++ app/src/test/java/br/com/dfn/app/sunsetexp/ExampleUnitTest.kt
@@ -1,7 +1,6 @@
 package br.com.dfn.app.sunsetexp
 
 import org.junit.Test
-
 import org.junit.Assert.assertEquals
 
 /**
--- feature/login/src/test/java/br/com/dfn/app/feature/login/ExampleUnitTest.kt
+++ feature/login/src/test/java/br/com/dfn/app/feature/login/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package br.com.dfn.app.feature.login
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
